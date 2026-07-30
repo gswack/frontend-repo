2fbf7aa4cf10fe776f22a392e887fae7fec11888
gswack <swack.gabe@gmail.com>
2026-07-30 18:27:36 +0300

    Frontend tag v5, fix HotelID - hotel name bug

--- src/screens/NewReservation.jsx
+++ src/screens/NewReservation.jsx
@@ -64,7 +64,7 @@ export default function NewReservation() {
             onChange={(e) => setSelectedHotel(e.target.value)}
           >
             {hotels.map((h) => (
-              <option key={h.id} value={h.name}>
+              <option key={h.id} value={h.id}>
                 {h.name}
               </option>
             ))}
